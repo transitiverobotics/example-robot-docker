31d444f2ca4f5139322b0ba99fc23b42f445157a
Christian Fritz <christian.fritz@gmail.com>
2023-06-27 12:07:31 -0700

    Modified to work with userid and token provided as arguments, published as image

--- run.sh
+++ run.sh
@@ -1,25 +1,20 @@
 #!/bin/bash
 
-# example usage:
-# ./run.sh ros:noetic
+# Simple script to build and run a docker container simulating a robot that
+# has the Transitive agent installed and connects to the account specified.
+#
+# Usage:
+# ./run.sh userId robotToken
 
 set -e
 
-BUILDARGS=""
-TAGNAME=robot
-if [[ $# > 0 ]]; then
-  TAGNAME+=_${1/:/-}
-  BUILDARGS+="--build-arg BASE_IMAGE=$1"
-fi;
+# --- build
+docker build -t transitiverobotics/try .
+
+# --- run
 
 # generate a random four-digit number
 NUMBER=$(tr -dc 0-9 < /dev/urandom | fold -w 4 | head -n 1)
-
-. .env
-BUILDARGS+=" --build-arg USERID=$USERID"
-BUILDARGS+=" --build-arg TOKEN=$TOKEN"
-
-docker build $BUILDARGS -t $TAGNAME .
 
 DIR=/tmp/transitive_$NUMBER
 mkdir -p $DIR
@@ -27,13 +22,8 @@ echo "TR_LABELS=docker" > $DIR/.env_user
 
 docker run -it --rm \
 --privileged \
---hostname robot_$TAGNAME_$NUMBER \
+--hostname robot_$NUMBER \
 -v $DIR:/root/.transitive \
 -v /run/udev:/run/udev \
 --name robot \
-$TAGNAME $2
-
-# --device=/dev/video0 \
-# -v /sys:/sys \
-# -v /dev:/dev \
-# -e UDEV=1 \
+transitiverobotics/try $1 $2
