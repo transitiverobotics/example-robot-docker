7b8f28cc6f4908696b2ae86b2b0f6ff9fc813ad4
Christian Fritz <christian.fritz@gmail.com>
2023-06-27 16:36:45 -0700

    Start a roscore

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,8 +10,6 @@ cd $HOME/.transitive
 # Optionall you can set a custom device name:
 # export TR_INSTALL_HASH=my_bot_name
 
-#HERE  if env vars for userid and token exist, write them to .env and .token
-
 if [[ ! -z $1 ]]; then
   sed -i "s/TR_USERID=.*/TR_USERID=$1/" .env;
 fi;
@@ -19,5 +17,8 @@ fi;
 if [[ ! -z $2 ]]; then
   echo "TR_ROBOT_TOKEN=$2" > .token;
 fi;
+
+. /opt/ros/noetic/setup.bash
+roscore &
 
 bash start_agent.sh
