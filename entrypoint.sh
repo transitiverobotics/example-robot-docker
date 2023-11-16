c41091cb1e5d8c69c18cdd853987cd78e55516e2
Christian Fritz <christian.fritz@gmail.com>
2023-11-16 12:06:20 -0800

    example-docker: support mDNS and local dev usage
    Fixes https://github.com/chfritz/transitive/issues/386

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ fi;
 
 cd $HOME/.transitive
 
-# Optionall you can set a custom device name:
+# Optionally you can set a custom device name:
 # export TR_INSTALL_HASH=my_bot_name
 
 if [[ ! -z $1 ]]; then
@@ -16,6 +16,27 @@ fi;
 
 if [[ ! -z $2 ]]; then
   echo "TR_ROBOT_TOKEN=$2" > .token;
+fi;
+
+if [[ ! -z $3 ]]; then
+  # A different install host was provided, mostly used for local dev.
+
+  # remove existing lines
+  sed -i '/^TR_INSTALL_HOST/d' .env;
+  sed -i '/^TR_HOST/d' .env;
+
+  # extract TR_HOST from install host
+  HOST=$(echo $3 | sed 's/https\{0,1\}:\/\/install.//')
+  echo "TR_INSTALL_HOST=$3" >> .env
+  echo "TR_HOST=$HOST" >> .env
+
+  if [[ "$3" == *local ]]; then
+    # See https://github.com/transitiverobotics/transitive/blob/main/cloud/tools/mDNS/README.md
+    echo "Using a .local install domain: adding mDNS support"
+    apt-get install -y avahi-utils
+    sed -i "s/mdns4_minimal/mdns4 mdns4_minimal/" /etc/nsswitch.conf
+    echo -e ".local.\n.local\n" >> /etc/mdns.allow
+  fi
 fi;
 
 . /opt/ros/noetic/setup.bash
