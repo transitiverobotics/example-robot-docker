a149180ec34696553d5fe4db3a9c42838d28efc8
Christian Fritz <christian.fritz@gmail.com>
2024-02-26 08:35:20 -0800

    Updated build_and_push script to set builder

--- build_and_push.sh
+++ build_and_push.sh
@@ -2,4 +2,7 @@
 
 # Script to build and push image for both amd64 and arm64
 
-docker buildx build --platform linux/amd64,linux/arm64 -t transitiverobotics/try --push .
+# Assumes you have already created a builder called `mybuilder` using:
+# docker buildx create --name mybuilder --bootstrap
+
+docker buildx build --builder mybuilder --platform linux/amd64,linux/arm64 -t transitiverobotics/try --push .
