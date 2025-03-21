450783bdff1bab2dd4959fe8f0e770229d5208f9
Javier Lurgo <jlurgo@gmail.com>
2025-03-21 14:39:16 -0300

    ros distro as a parameter

--- build_and_push.sh
+++ build_and_push.sh
@@ -5,4 +5,13 @@
 # Assumes you have already created a builder called `mybuilder` using:
 # docker buildx create --name mybuilder --bootstrap
 
-docker buildx build --builder mybuilder --platform linux/amd64,linux/arm64 -t transitiverobotics/try --push .
+# first arg is ros distro
+ROS_DISTRO=$1
+
+
+docker buildx build \
+    --builder mybuilder \
+    --platform linux/amd64,linux/arm64 \
+    --build-arg ROS_DISTRO=$ROS_DISTRO \
+    -t transitiverobotics/try \
+    --push .
