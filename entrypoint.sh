b9fbfe1ba28b3d42cf04a7c2d1ad6b52e728bb14
Javier Lurgo <jlurgo@gmail.com>
2025-03-26 17:28:35 -0300

    image name including ros distribution

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,28 +10,28 @@ cd $HOME/.transitive
 # Optionally you can set a custom device name:
 # export TR_INSTALL_HASH=my_bot_name
 
+if [[ ! -z $1 ]]; then
+  sed -i "s/TR_USERID=.*/TR_USERID=$1/" .env;
+fi;
+
 if [[ ! -z $2 ]]; then
-  sed -i "s/TR_USERID=.*/TR_USERID=$2/" .env;
+  echo "TR_ROBOT_TOKEN=$2" > .token;
 fi;
 
 if [[ ! -z $3 ]]; then
-  echo "TR_ROBOT_TOKEN=$3" > .token;
-fi;
-
-if [[ ! -z $4 ]]; then
   # A different install host was provided, mostly used for local dev. Update
   # .env and .npmrc and setup mDNS support. Compare to install.sh.
 
   # extract TR_HOST from install host
-  HOST=$(echo $4 | sed 's/https\{0,1\}:\/\/install.//')
+  HOST=$(echo $3 | sed 's/https\{0,1\}:\/\/install.//')
 
   # Update .env
   sed -i '/^TR_INSTALL_HOST/d' .env;
   sed -i '/^TR_HOST/d' .env;
-  echo "TR_INSTALL_HOST=$4" >> .env
+  echo "TR_INSTALL_HOST=$3" >> .env
   echo "TR_HOST=$HOST" >> .env
 
-  if [[ "$4" == *local ]]; then
+  if [[ "$3" == *local ]]; then
     # See https://github.com/transitiverobotics/transitive/blob/main/cloud/tools/mDNS/README.md
     echo "Using a .local install domain: adding mDNS support"
     sed -i "s/mdns4_minimal/mdns4 mdns4_minimal/" /etc/nsswitch.conf
@@ -39,9 +39,9 @@ if [[ ! -z $4 ]]; then
   fi
 
   # update .npmrc from install host (this requires mDNS)
-  curl -sf $4/files/.npmrc -o .npmrc
+  curl -sf $3/files/.npmrc -o .npmrc
 
 fi;
 
-. /opt/ros/$1/setup.bash
+. /opt/ros/*/setup.bash
 bash start_agent.sh
